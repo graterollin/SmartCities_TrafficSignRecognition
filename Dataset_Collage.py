729e9961479c50db150b1d50ceb7f65fb6ea006a
graterollin <77891063+graterollin@users.noreply.github.com>
2022-01-12 12:12:37 -0500

    Delete Dataset_Collage.py
    deprecated

--- Dataset_Collage.py
+++
@@ -1,8 +0,0 @@
-# Author: Andres Graterol 
-# Date: 06/17/21
-# This code is for visualizing datasets using HTML rendering
-
-# Methods for visually inspecting data:
-# 1. jupyter notebook using matplotlib
-# 2. Use dedicated tooling like google facets to explore image data 
-# 3. use HTML rendering to visualize and explore in a notebook
